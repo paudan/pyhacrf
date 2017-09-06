4d82e51d06cb83ff6686c2c6b814fbd7d2df5577
Forest Gregg <fgregg@uchicago.edu>
2017-09-06 11:57:16 -0500

    pyproject

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 import numpy
 
 # Get the long description from the relevant file
-with open('README.rst'), encoding='utf-8') as f:
+with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 
